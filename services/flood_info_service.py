4a24cf30769548d0bcac597683312496e81b3453
Lishin <99582713+Lishin1215@users.noreply.github.com>
2025-03-07 17:00:44 +0000

    finish get_particular_M function
    in both app.py and flood_info_service

--- services/flood_info_service.py
+++ services/flood_info_service.py
@@ -28,12 +28,26 @@ class FloodInfoService:
         except requests.RequestException as e:
             logging.error(f"API request failed for station {station_id}: {str(e)}")
             return {"error":"API request failed"}
+        
+    
+    def get_particular_M(self, notation):
+        try:
+            response = requests.get(f"{self.API_URL}/id/measures/{notation}/readings")
+            response.raise_for_status
+            data = response.json()
+
+            return data
+        except requests.RequestException as e:
+            logging.error(f"API request failed for station {notation}: {str(e)}")
+            return {"error": "API request failed"}
+
 
 
 
 f = FloodInfoService()
 # print(f.get_stations())
-print(f.get_measurement(station_id="1029TH"))
+# print(f.get_measurement(station_id="1029TH"))
+print(f.get_particular_M(notation = "F1906-flow-logged-i-15_min-m3_s"))
   
 
     
