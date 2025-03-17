5600e2d97b03ee66f2159e724871b957a4ffaff1
Lishin <99582713+Lishin1215@users.noreply.github.com>
2025-03-17 00:32:14 +0000

    fix port listen

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def get_particular_M(notation):
         return jsonify({"error": "Failed to fetch measurement data"}), 500
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
 
 
     
